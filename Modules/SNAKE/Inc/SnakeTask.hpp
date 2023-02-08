f3789ca92080fd1b306037938b82ce8607cd69d7
XForgivenGitX <ilkubarev1805@gmail.com>
2023-02-08 21:25:18 +0300

    08.02.2023

--- Modules/SNAKE/Inc/SnakeTask.hpp
+++ Modules/SNAKE/Inc/SnakeTask.hpp
@@ -5,7 +5,7 @@
 constexpr uint32_t SIZE_SIDE_SQUARE = 15;
 constexpr uint32_t RADIUS_ROUNDING = 2;
 constexpr uint32_t INIT_SPEED_GAME = 200;
-constexpr uint32_t FINAL_SPEED_GAME = 50;
+constexpr uint32_t FINAL_SPEED_GAME = 100;
 //namespace snake{
 
 
