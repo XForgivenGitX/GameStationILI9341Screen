7ddfa6aff4ef4b0662937700b976033c587024ba
XForgivenGitX <ilkubarev1805@gmail.com>
2023-02-08 22:46:12 +0300

    ...

--- Modules/BUTTONS/Src/but_controlBlock.cpp
+++ Modules/BUTTONS/Src/but_controlBlock.cpp
@@ -4,35 +4,35 @@ using namespace but;
 std::list<ButtonWrapper*> ButtonControlBlock::myButtons;
 
 ButtonWrapper* ButtonControlBlock::CreateButton(GPIO_TypeDef* Port, uint16_t Pin, GPIO_PinState Default)
-	{
-		ButtonWrapper* newButton = new ButtonWrapper(Port, Pin, Default);
-		myButtons.push_back(newButton);
-		return newButton;
-	}
+{
+	ButtonWrapper* newButton = new ButtonWrapper(Port, Pin, Default);
+	myButtons.push_back(newButton);
+	return newButton;
+}
 
 ////////////////////////////////////////////////////////
 void ButtonControlBlock::ButtonHandler()
+{
+	for(auto it : myButtons)
 	{
-		for(auto it : myButtons)
+		bool PinState = it->ReadPin();
+		switch(it->ButtonState_)
 		{
-			bool PinState = it->ReadPin();
-			switch(it->ButtonState_)
-			{
-			case ButtonState::NEVERMIND:
-				it->NevermindHandle(PinState);
-				break;
+		case ButtonState::NEVERMIND:
+			it->NevermindHandle(PinState);
+			break;
 
-			case ButtonState::PRESS_BOUNCE:
-				it->PressBounceHandle(PinState);
-				break;
+		case ButtonState::PRESS_BOUNCE:
+			it->PressBounceHandle(PinState);
+			break;
 
-			case ButtonState::SLICE_WAIT:
-				it->SliceWaitHandle(PinState);
-				break;
+		case ButtonState::SLICE_WAIT:
+			it->SliceWaitHandle(PinState);
+			break;
 
-			case ButtonState::RELEASE_BOUNCE:
-				it->ReleaseBounceHandle(PinState);
-				break;
-			}
+		case ButtonState::RELEASE_BOUNCE:
+			it->ReleaseBounceHandle(PinState);
+			break;
 		}
 	}
+}
